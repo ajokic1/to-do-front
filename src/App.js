e0f2510abcabe30aa02ebb62141d95cd7b25c5da
Andrej Jokić <aj.jokic@gmail.com>
2020-09-24 13:18:03 +0200

    #LAGK-35 Minor fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import UserContext from "./auth/UserContext";
 import Router from "./Router";
